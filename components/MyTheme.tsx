62d1f0d2980618d95a6ae79a8bb05af30c3614eb
ayoub-root <a.benayache@univ-batna2.dz>
2024-06-13 01:57:22 +0200

    new domaine devwithx wch home page245

--- components/MyTheme.tsx
+++ components/MyTheme.tsx
@@ -113,46 +113,10 @@ export default function MyTheme(props: { children: React.ReactNode }) {
     return (
         <ThemeProvider theme={LPtheme}>
 
-            <Head>
-
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon"
-                      type="image/png" sizes="60x60" href="/images/icons/favicon.png"/>
-                <title> Develop With X</title>
-                <meta name="description" content=" "/>
-                <meta name="robots" content="index, follow"/>
-                <meta charSet="UTF-8"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                <title>Your Website Title</title>
-
-                <meta name="environment" content="production"/>
-                <meta name="description"
-                      content="Discover insightful software programming tutorials, tips, and solutions on my personal blog. Whether you're a beginner or an experienced developer, find practical advice, in-depth guides, and problem-solving techniques to enhance your coding skills and advance your projects. Join our community and stay updated with the latest trends and best practices in the world of software development."/>
-
-
-                <meta property="og:title" content="Your Website Title"/>
-                <meta property="og:type" content="website"/>
-                <meta property="og:url" content="https://devwithx.com/"/>
-                <meta property="og:image" content="https://devwithx.com/images/img/presentation3.png"/>
-                <meta property="og:description"
-                      content="Discover insightful software programming tutorials, tips, and solutions on my personal blog. Whether you're a beginner or an experienced developer, find practical advice, in-depth guides, and problem-solving techniques to enhance your coding skills and advance your projects. Join our community and stay updated with the latest trends and best practices in the world of software development."/>
-                <meta property="og:site_name" content="Develop with X"/>
-                <meta property="og:locale" content="en_US"/>
-
-
-                <meta name="twitter:card" content="summary_large_image"/>
-                <meta name="twitter:title" content="Develop with X"/>
-                <meta name="twitter:description"
-                      content="Discover insightful software programming tutorials, tips, and solutions on my personal blog. Whether you're a beginner or an experienced developer, find practical advice, in-depth guides, and problem-solving techniques to enhance your coding skills and advance your projects. Join our community and stay updated with the latest trends and best practices in the world of software development."/>
-                <meta name="twitter:image" content="https://devwithx.com/images/img/presentation3.png"/>
-                <meta name="twitter:site" content="@yourtwitterhandle"/>
-                <meta name="twitter:creator" content="@yourtwitterhandle"/>
-
-
-            </Head>
 
             <SnackbarProvider maxSnack={3}>
                 <>
+
                     <div>{props.children} </div>
 
                     <Drawer open={openLogin || openRegister || openReset || openForgot}
